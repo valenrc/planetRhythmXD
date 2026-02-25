8379a9021412e5a0bad740cc6bc36c3657f9eec6
valenrc <vvalenrc@gmail.com>
2026-02-25 12:21:46 -0300

    funcionalidad basica del conductor

--- conductor.gd
+++ conductor.gd
@@ -5,12 +5,12 @@ extends AudioStreamPlayer
 @export var measures = 4  # compases de la canción
 
 # Variables internas
-var song_position:float = 0.0	# tiempo desde el inicio de la canción
+var song_position:float = 0.0	# tiempo desde el inicio de la canción (en segundos)
 var song_position_in_beats:int = 1 # cantidad de beats desde el inicio de la canción (relativo a los bpm)
 var sec_per_beat:float = 60.0 / bpm	# cantidad de segundos entre cada beat en la canción
 var last_reported_beat:int = 0	# último beat detectado
 var beats_before_start:int = 0	# cantidad de beats para simular antes de que empiece la canción
-var curr_measure:int = 0	 # compás actual
+var current_measure:int = 0	 # compás actual
 
 # Determining how close to the beat an event is
 var closest = 0.0
@@ -32,10 +32,18 @@ func _process(delta: float) -> void:
 		song_position -= AudioServer.get_output_latency()
 		
 		# convierto el timing actual a la cantidad de beats ocurridos desde el inicio de la canción
-		song_position_in_beats = int(floor(song_position / sec_per_beat)) + beats_before_start
-		_report_beat()
+		#song_position_in_beats = int(floor(song_position / sec_per_beat)) + beats_before_start
+		#_report_beat()
 		
 func _report_beat():
 	if song_position_in_beats > last_reported_beat:
-		print("time: ",song_position, " time in beats: ", song_position_in_beats)
+		if current_measure > measures:
+			current_measure = 1
+		
+		emit_signal("beat", song_position_in_beats)
+		emit_signal("measure", current_measure)
+
 		last_reported_beat = song_position_in_beats
+		current_measure += 1
+
+		#print("time: ",song_position, " time in beats: ", song_position_in_beats)
